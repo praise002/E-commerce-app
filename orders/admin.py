fd70d69cc1c2a6972197bb7519346d7cc7808990
Praise Idowu <ifeoluwapraise02@gmail.com>
2023-01-03 05:28:05 -0600

    Exporting orders to csv files

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,35 @@
+import csv 
+import datetime
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 from . models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+    
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')  #the response has to be treated as a csv file
+    response['content-Disposition'] = content_disposition  #the httpresponse contains an attached file
+    writer = csv.writer(response)  #write to the response obj
+    fields = [field for field in opts.get_fields() if not \
+        field.many_to_many and not field.one_to_many]  #exclude many-to-many & one-to-many
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    #Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'  #customize the display name
     
 def order_payment(obj):
     url = obj.get_stripe_url()
@@ -22,3 +47,4 @@ class OrderAdmin(admin.ModelAdmin):
                             order_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
